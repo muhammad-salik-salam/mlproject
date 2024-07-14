44e92ea920c8a8824213c3beac7b2d6c15ca1ef4
imsalik <salam.salik96@gmail.com>
2024-07-14 00:05:14 -0500

    model trainer done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,8 +5,10 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -42,4 +44,6 @@ if __name__=="__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation= DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    model_train= ModelTrainer()
+    print(model_train.initiate_model_training(train_arr, test_arr))
